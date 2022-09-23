15cbd1273e5a0f8fd96ff488f9f37645e31262ad
mg143pavankumar <pavansharma.mg143@gmail.com>
2022-09-23 21:27:28 +0530

    added functionality for detecting unknown person

--- main.py
+++ main.py
@@ -93,6 +93,15 @@ while True:
 
             # mark attendance to particular person
             markAttendance(name)
+        else:
+            name = "Unknown"
+            y1, x2, y2, x1 = faceLocation
+            y1, x2, y2, x1 = y1*4, x2*4, y2*4, x1*4
+            cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
+            cv2.rectangle(img, (x1, y2 - 35), (x2, y2),
+                          (0, 255, 0), cv2.FILLED)
+            cv2.putText(img, name, (x1 + 6, y2 - 6),
+                        cv2.FONT_HERSHEY_DUPLEX, 0.7, (255, 255, 255), 1)
 
     cv2.imshow('Webcam', img)
     cv2.waitKey(1)
